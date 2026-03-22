647404bfa832c2341d638ac2837ad688d19c869e
Hong-203 <danghong0521@gmail.com>
2026-03-22 11:04:01 +0700

    feat(ui) reponsive

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        // Add a dummy reducer to avoid Redux error if no real reducers exist yet
+        _dummy: (state = {}) => state,
+    },
 });
 
 export default store;
